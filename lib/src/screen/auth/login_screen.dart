ffc5aa4f87945629b438b4ef930b2ac5d546efe3
letitgobaby <gnu@gnuui-MacBookPro.local>
2023-12-30 21:16:42 +0900

    feat : Login 화면 수정

--- lib/src/screen/auth/login_screen.dart
+++ lib/src/screen/auth/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:greatedays/src/provider/auth_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:sizer/sizer.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,16 +11,54 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  //
+
+  Widget googleLogin() {
+    return InkWell(
+      onTap: () {
+        context.read<AuthProvider>().storeToken("mock-user-token");
+      },
+      child: Container(
+        height: 40,
+        width: 200,
+        margin: const EdgeInsets.all(8),
+        color: Colors.red[100],
+        child: const Center(child: Text("GOOGLE LOGIN")),
+      ),
+    );
+  }
+
+  Widget appleLogin() {
+    return InkWell(
+      onTap: () {
+        context.read<AuthProvider>().storeToken("mock-user-token");
+      },
+      child: Container(
+        height: 40,
+        width: 200,
+        margin: const EdgeInsets.all(8),
+        color: Colors.blue[100],
+        child: const Center(child: Text("APPLE LOGIN")),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: null,
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            context.read<AuthProvider>().storeToken("mock-user-token");
-          },
-          child: const Text("login"),
+      body: SizedBox(
+        height: 100.h,
+        width: 100.w,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(Icons.logo_dev_outlined, size: 100),
+              googleLogin(),
+              appleLogin(),
+            ],
+          ),
         ),
       ),
     );
